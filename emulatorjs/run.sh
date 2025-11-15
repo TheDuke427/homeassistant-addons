4cfe9f2a0eb39ff0a134d43052fd9150ae8c1322
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-14 20:48:22 -0800

    Update run.sh

--- emulatorjs/run.sh
+++ emulatorjs/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd /emulatorjs
 
-# Start http-server on all interfaces
-npx http-server /roms -p 8080 -a 0.0.0.0
+# Make sure EmulatorJS is built
+npm install
+npm run build
+
+# Start http-server to serve the UI
+npx http-server /emulatorjs/dist -p 8080 -a 0.0.0.0
