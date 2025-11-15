60104477755c632c280a3cd0871a036ff026b029
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-14 19:53:38 -0800

    Update run.sh

--- emulatorjs/run.sh
+++ emulatorjs/run.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-# Run EmulatorJS in HA container
-
-# Navigate to emulatorjs directory
 cd /emulatorjs
 
-# Start the server
-# Adjust port if needed
-node server.js
+# Start EmulatorJS using npm start
+npm start
