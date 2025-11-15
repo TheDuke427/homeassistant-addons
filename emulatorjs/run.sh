440d1e3591d8a770f7a412ad318a54853c0a3493
TheDuke427 <56452519+TheDuke427@users.noreply.github.com>
2025-11-14 21:36:51 -0800

    Update run.sh

--- emulatorjs/run.sh
+++ emulatorjs/run.sh
@@ -5,5 +5,8 @@ cd /emulatorjs
 npm install
 npm run build
 
+# Extract archive to dist
+7z x 4.2.3.7z -o./dist
+
 # Start http-server to serve the UI
-npx http-server /emulatorjs/dist -p 8080 -a 0.0.0.0
+npx http-server ./dist -p 8080 -a 0.0.0.0
